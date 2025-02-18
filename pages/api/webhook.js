54047060d5f8fbf2b1a314adbf891811875d5d86
ephantus <muephanzz@gmail.com>
2025-02-18 22:53:31 +0300

    web

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,4 +1,4 @@
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
