2bf53264e97cf7cab019edf2b11ef6ba1b5c867c
ephantus <muephanzz@gmail.com>
2025-03-25 19:26:17 +0300

    poo

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -2,9 +2,18 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function ProductCard({ product }) {
+  const price = product.price;
+  const discount = price <= 200 ? 20 : 0; // 20% discount for products under Ksh 200
+  
   return (
     <Link href={`/products/${product.product_id}`} key={product.product_id} className="group">
-      <div className="border rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300">
+      <div className="border relative rounded-lg shadow-md p-2 sm:p-4 hover:shadow-lg transition duration-300">
+          <h4 className="absolute top-0 left-0 bg-gray-500 text-white rounded px-2">{product.state}</h4>
+          {product.price <= 200 && (
+            <h4 className="absolute left-0 top-5 text-red-600 font-bold animate-shake">
+              {discount}% discount
+            </h4>
+          )}
         {/* Display First Image Only */}
         <Image
           src={product.image_urls?.[0] || "/placeholder.jpg"}
@@ -14,16 +23,10 @@ export default function ProductCard({ product }) {
           height={500}
           unoptimized // If using external URLs like Supabase
         />
-        {product.price < 200 && (
-          <span className="absolute left-3 top-9 text-red-600 font-bold animate-shake">
-            20% OFFER
-          </span>
-        )}
-        <h4 className="absolute bg-gray-500 text-white top-3 left-3 rounded px-2">{product.state}</h4>
         <h3 className="mt-3 text-lg font-semibold text-gray-800 group-hover:text-blue-600 transition">
           {product.name}
         </h3>
-        <p className="flex">{product.description.split(" ").slice(0, 2).join(" ")}...</p>
+       <p className="flex">{product.description.split(" ").slice(0, 2).join(" ")}...</p>
         <p className="mt-1 font-bold text-blue-600">Ksh {product.price}</p>
       </div>
     </Link>
