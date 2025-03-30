ac5e15bc3cc85a7ab8417cee2a77f18d0695beea
ephantus <muephanzz@gmail.com>
2025-03-30 17:21:20 +0300

    deploying and correcting the errores in app pages

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabaseClient";
-import { ProductCard } from "./components/ProductCard";
+import ProductCard from "./components/ProductCard";
 import { Loader2 } from "lucide-react";
 import Pagination from "./components/Pagination";
 import Footer from "./components/Footer";
