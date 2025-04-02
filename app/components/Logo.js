c0a371b9c9525b0b4c914ef7fb6c4fcfd36cf01d
ephantus <muephanzz@gmail.com>
2025-04-02 20:47:56 +0300

    loploplioplp

--- app/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Logo() {
-  return (
-    <Link href="/">
-      <h1 className="text-2xl font-bold text-blue-600 hidden md:block">Ephantronics</h1>
-    </Link>
-  );
-}
--- app/not-found/page.js
+++
@@ -1,6 +0,0 @@
-import { notFound } from "next/navigation";
-
-export default function Page({ params }) {
-  if (!params.id) return notFound();
-  return <h1>Product Page</h1>;
-}
