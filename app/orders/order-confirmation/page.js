22b855cc970f751d52cf5a799b802af52395ccfc
ephantus <muephanzz@gmail.com>
2025-04-02 17:08:04 +0300

    orders

--- app/orders/order-confirmation/page.js
+++
@@ -1,9 +0,0 @@
-export default function OrderConfirmation() {
-    return (
-      <div className="mt-30 text-center">
-        <h1 className="text-2xl font-bold">Payment Received!</h1>
-        <p>Your order is being processed.</p>
-      </div>
-    );
-  }
-  
