06904df6212223695ec4c3c6e6537a89b973f487
ephantus <muephanzz@gmail.com>
2025-04-02 21:15:34 +0300

    charlitr

---
+++ app/lib/user.js
@@ -0,0 +1,18 @@
+"use client";
+import { useEffect, useState } from 'react';
+import { supabase } from './supabaseClient';
+
+export const useUser = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data: { user }, error } = await supabase.auth.getUser();
+      if (!error) setUser(user);
+    };
+
+    fetchUser();
+  }, []);
+
+  return { user };
+};
