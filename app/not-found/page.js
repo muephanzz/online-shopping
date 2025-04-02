307d373dbb72ab6de2ef057806aaaf1d672bb045
ephantus <muephanzz@gmail.com>
2025-04-02 18:24:22 +0300

    not found

--- app/not-found/page.js
+++ app/not-found/page.js
@@ -1,3 +1,6 @@
-export default function NotFound() {
-  return null;
+import { notFound } from "next/navigation";
+
+export default function Page({ params }) {
+  if (!params.id) return notFound();
+  return <h1>Product Page</h1>;
 }
